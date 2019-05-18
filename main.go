02635cdd142863ccac0f894bd17f9fb2a2c90da7
Araquach <adam@contra-design.com>
2019-05-18 16:57:38 +0100

    Added the team pages

--- main.go
+++ main.go
@@ -7,10 +7,22 @@ import (
 )
 
 var homeView *views.View
+var teamView *views.View
+var team_indView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(homeView.Render(w, nil))
+}
+
+func team(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(teamView.Render(w, nil))
+}
+
+func team_ind(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(team_indView.Render(w, nil))
 }
 
 func must(err error) {
@@ -21,8 +33,12 @@ func must(err error) {
 
 func main() {
 	homeView = views.NewView("main", "views/home.gohtml")
+	teamView = views.NewView("main", "views/team.gohtml")
+	team_indView = views.NewView("main", "views/team_ind.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/team", team)
+	r.HandleFunc("/team_ind", team_ind)
 	http.ListenAndServe(":3000", r)
 }
