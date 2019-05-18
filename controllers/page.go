750dedd0c0d14fc7d6e29586a3305d9605bae7bf
Araquach <adam@contra-design.com>
2019-05-18 21:10:26 +0100

    Refactored rverything nice!

---
+++ controllers/page.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"jakataGo/views"
+)
+
+func NewPage() *Page {
+	return &Page{
+		HomeView: views.NewView("main", "views/pages/home.gohtml"),
+		ContactView: views.NewView("main", "views/pages/contact.gohtml"),
+		BlogView: views.NewView("main", "views/pages/blog.gohtml"),
+		BlogIndView: views.NewView("main", "views/pages/blog.gohtml"),
+		DetailsView: views.NewView("main", "views/pages/details.gohtml"),
+		PricesView: views.NewView("main", "views/pages/prices.gohtml"),
+		RecruitmentView: views.NewView("main", "views/pages/recruitment.gohtml"),
+		ReviewsView: views.NewView("main", "views/pages/reviews.gohtml"),
+		SalonView: views.NewView("main", "views/pages/salon.gohtml"),
+		TeamView: views.NewView("main", "views/pages/team.gohtml"),
+		TeamIndView: views.NewView("main", "views/pages/team_ind.gohtml"),
+	}
+}
+
+type Page struct {
+	HomeView *views.View
+	ContactView *views.View
+	BlogView *views.View
+	BlogIndView *views.View
+	DetailsView *views.View
+	PricesView *views.View
+	RecruitmentView *views.View
+	ReviewsView *views.View
+	SalonView *views.View
+	TeamView *views.View
+	TeamIndView *views.View
+}
--- main.go
+++ main.go
@@ -2,108 +2,25 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"jakataGo/controllers"
 	"net/http"
-	"static-site/views"
 )
 
-var homeView *views.View
-var teamView *views.View
-var team_indView *views.View
-var blogView *views.View
-var blog_indView *views.View
-var detailsView *views.View
-var contactView *views.View
-var reviewsView *views.View
-var salonView *views.View
-var pricesView *views.View
-var recruitmentView *views.View
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func team(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(teamView.Render(w, nil))
-}
-
-func team_ind(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(team_indView.Render(w, nil))
-}
-
-func blog(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(blogView.Render(w, nil))
-}
-
-func blog_ind(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(blog_indView.Render(w, nil))
-}
-
-func details(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(detailsView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func reviews(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(reviewsView.Render(w, nil))
-}
-
-func salon(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(salonView.Render(w, nil))
-}
-
-func prices(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(pricesView.Render(w, nil))
-}
-
-func recruitment(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(recruitmentView.Render(w, nil))
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	homeView = views.NewView("main", "views/home.gohtml")
-	teamView = views.NewView("main", "views/team.gohtml")
-	team_indView = views.NewView("main", "views/team_ind.gohtml")
-	blogView = views.NewView("main", "views/blog.gohtml")
-	blog_indView = views.NewView("main", "views/blog_ind.gohtml")
-	detailsView = views.NewView("main", "views/details.gohtml")
-	contactView = views.NewView("main", "views/contact.gohtml")
-	reviewsView = views.NewView("main", "views/reviews.gohtml")
-	salonView = views.NewView("main", "views/salon.gohtml")
-	pricesView = views.NewView("main", "views/prices.gohtml")
-	recruitmentView = views.NewView("main", "views/recruitment.gohtml")
+	pageC := controllers.NewPage()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/team", team)
-	r.HandleFunc("/team_ind", team_ind)
-	r.HandleFunc("/blog", blog)
-	r.HandleFunc("/blog_ind", blog_ind)
-	r.HandleFunc("/details", details)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/reviews", reviews)
-	r.HandleFunc("/salon", salon)
-	r.HandleFunc("/prices", prices)
-	r.HandleFunc("/recruitment", recruitment)
+	r.Handle("/", pageC.HomeView).Methods("GET")
+	r.Handle("/team", pageC.TeamView).Methods("GET")
+	r.Handle("/team_ind", pageC.TeamIndView).Methods("GET")
+	r.Handle("/blog", pageC.BlogView).Methods("GET")
+	r.Handle("/blog_ind", pageC.BlogIndView).Methods("GET")
+	r.Handle("/details", pageC.DetailsView).Methods("GET")
+	r.Handle("/contact", pageC.ContactView).Methods("GET")
+	r.Handle("/reviews", pageC.ReviewsView).Methods("GET")
+	r.Handle("/salon", pageC.SalonView).Methods("GET")
+	r.Handle("/prices", pageC.PricesView).Methods("GET")
+	r.Handle("/recruitment", pageC.RecruitmentView).Methods("GET")
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3010", r)
 }
