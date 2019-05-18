53c3057502ce035c32ecb8a5c365750895e67e07
Araquach <adam@contra-design.com>
2019-05-18 17:15:30 +0100

    Added Blog Pages

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 var homeView *views.View
 var teamView *views.View
 var team_indView *views.View
+var blogView *views.View
+var blog_indView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -25,6 +27,16 @@ func team_ind(w http.ResponseWriter, r *http.Request) {
 	must(team_indView.Render(w, nil))
 }
 
+func blog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(blogView.Render(w, nil))
+}
+
+func blog_ind(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(blog_indView.Render(w, nil))
+}
+
 func must(err error) {
 	if err != nil {
 		panic(err)
@@ -35,10 +47,14 @@ func main() {
 	homeView = views.NewView("main", "views/home.gohtml")
 	teamView = views.NewView("main", "views/team.gohtml")
 	team_indView = views.NewView("main", "views/team_ind.gohtml")
+	blogView = views.NewView("main", "views/blog.gohtml")
+	blog_indView = views.NewView("main", "views/blog_ind.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/team", team)
 	r.HandleFunc("/team_ind", team_ind)
+	r.HandleFunc("/blog", blog)
+	r.HandleFunc("/blog_ind", blog_ind)
 	http.ListenAndServe(":3000", r)
 }
