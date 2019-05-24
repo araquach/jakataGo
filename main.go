a64c33d91e7782a478ee763070b8558965bd4919
Araquach <adam@contra-design.com>
2019-05-24 08:22:05 +0100

    Updated

--- main.go
+++ main.go
@@ -8,20 +8,20 @@ import (
 
 func main() {
 
-	pageC := controllers.NewPage()
+		pageC := controllers.NewPage()
 
-	r := mux.NewRouter()
-	r.Handle("/", pageC.HomeView).Methods("GET")
-	r.Handle("/team", pageC.TeamView).Methods("GET")
-	r.Handle("/team_ind", pageC.TeamIndView).Methods("GET")
-	r.Handle("/blog", pageC.BlogView).Methods("GET")
-	r.Handle("/blog_ind", pageC.BlogIndView).Methods("GET")
-	r.Handle("/details", pageC.DetailsView).Methods("GET")
-	r.Handle("/contact", pageC.ContactView).Methods("GET")
-	r.Handle("/reviews", pageC.ReviewsView).Methods("GET")
-	r.Handle("/salon", pageC.SalonView).Methods("GET")
-	r.Handle("/prices", pageC.PricesView).Methods("GET")
-	r.Handle("/recruitment", pageC.RecruitmentView).Methods("GET")
+		r := mux.NewRouter()
+		r.Handle("/", pageC.HomeView).Methods("GET")
+		r.Handle("/team", pageC.TeamView).Methods("GET")
+		r.Handle("/team_ind", pageC.TeamIndView).Methods("GET")
+		r.Handle("/blog", pageC.BlogView).Methods("GET")
+		r.Handle("/blog_ind", pageC.BlogIndView).Methods("GET")
+		r.Handle("/details", pageC.DetailsView).Methods("GET")
+		r.Handle("/contact", pageC.ContactView).Methods("GET")
+		r.Handle("/reviews", pageC.ReviewsView).Methods("GET")
+		r.Handle("/salon", pageC.SalonView).Methods("GET")
+		r.Handle("/prices", pageC.PricesView).Methods("GET")
+		r.Handle("/recruitment", pageC.RecruitmentView).Methods("GET")
 
 	// Styles
 	assetHandler := http.FileServer(http.Dir("./public/css/"))
